8b2a262912579505157b3b12d3fcbbcc9943a057
Andre Bossard <abossard@gmail.com>
2012-09-02 06:27:06 -0400

    nice

--- server.py
+++ server.py
@@ -4,14 +4,26 @@ from werkzeug.wrappers import Request, Response
 from werkzeug.debug import DebuggedApplication
 import os
 
+HOST=os.environ['IP']
+PORT=int(os.environ['PORT'])
+
 from flask import Flask
 app = Flask(__name__)
 app.debug = True
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello World 2!"
 
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
 
 class MonitorMiddleware(object):
     "Example middleware that appends a message to all 200 html responses"
@@ -20,8 +32,7 @@ class MonitorMiddleware(object):
     def __call__(self, environ, start_response):
         return self.app(environ, start_response)
 
-app = MonitorMiddleware(app)
+app.wsgi_app = MonitorMiddleware(app.wsgi_app)
 
-httpd = make_server(os.environ['IP'], int(os.environ['PORT']), app)
-
-httpd.handle_request()
+if __name__ == '__main__':
+    app.run(host=HOST, port=PORT)
